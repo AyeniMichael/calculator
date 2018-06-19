06f1b2e0faf061469d27f1726964d91add249a4c
Ayeni Michael <ayenimichael1998@gmail.com>
2018-06-19 16:33:01 +0000

    This commit will add a scripts repository, and index.html

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+function add(number1, number2){
+    return number1 + number2;
+}
+
+var number1 = (prompt("Enter the first number"));
+var number2 = (prompt("Enter the second number"));
+alert(add(number1, number2));
